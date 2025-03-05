b39b4e1c52cf6e9548fd6d12b10518550dc3d0f7
Rishu Kumar <rishukumar9233@gmail.com>
2025-03-05 13:22:00 +0530

    added user register and login in backend

---
+++ Backend/src/controllers/socketManager.js
@@ -0,0 +1,24 @@
+import { Server } from "Socket.io";
+
+const connectToSocket = (server) => {
+  const io = new Server(server);
+  io.on("connection", (socket) => {
+
+    socket.on("join-call", (path)=>{
+      
+    });
+    
+    socket.on("signal", (toId, message)=>{
+      io.to(toId).emit("signal", socket.id, message);
+    })
+
+    socket.on("chat-message", (data, sender)=>{
+    })
+
+    socket.on("disconnect", ()=>{
+    })
+    
+  });
+  return io;
+};
+export default connectToSocket;
